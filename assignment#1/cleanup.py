64c0a91b6df5af0a7a69c37cae437f4ebffeb6df
nocmok <nyusafonov@edu.hse.ru>
2021-02-18 19:42:38 +0300

    cleanup.py

---
+++ assignment#1/cleanup.py
@@ -0,0 +1,38 @@
+import sys
+import numpy as np
+import pandas as pd
+import re
+
+dict_path = ""
+output_path = ""
+
+if __name__ != "__main__":
+    sys.exit()
+
+
+def parse_argv():
+    global dict_path
+    global output_path
+    if(len(sys.argv) != 3):
+        print(
+            'usage: python cleanup.py [path/to/dictionary] [output/file/path]')
+        sys.exit()
+    dict_path = sys.argv[1]
+    output_path = sys.argv[2]
+
+cyr_pattern_str = "^[абвгдеёжзийклмнопрстуфхцчшщъыьэюя]+$" 
+cyr_pattern = re.compile(cyr_pattern_str)
+
+def drop_word(word):
+    return cyr_pattern.match(word) is None
+
+def cleanup():
+    dict_ = pd.read_table(dict_path, names=['Word', 'Freq', 'FreqPM'])
+    rows_before = dict_.size
+    dict_ = dict_[~dict_['Word'].transform(lambda w : drop_word(w))]
+    rows_after = dict_.size
+    print(f'{rows_before - rows_after} rows dropped')
+    dict_.to_csv(output_path)
+
+parse_argv()
+cleanup()
